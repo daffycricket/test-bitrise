562726e55e6f32838ac232b964722fb0fb26eccf
daffycricket <daffycricket@yahoo.fr>
2017-04-01 16:02:47 +0200

    fixed intro

--- app/src/main/java/nla/org/test_bitrise/MainIntroActivity.java
+++ app/src/main/java/nla/org/test_bitrise/MainIntroActivity.java
@@ -1,9 +1,7 @@
 package nla.org.test_bitrise;
 
-import nla.org.test_bitrise.R;
 import android.Manifest;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
@@ -12,17 +10,25 @@ import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 public class MainIntroActivity extends IntroActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         addSlide(new SimpleSlide.Builder()
                 .title(R.string.title_simple)
                 .description(R.string.description)
-                .image(R.drawable.ic_launcher)
+                .image(R.mipmap.ic_launcher)
                 .background(R.color.background)
                 .backgroundDark(R.color.background_dark)
                 .scrollable(false)
-                .permission(Manifest.permission.CAMERA)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_simple)
+                .description(R.string.description)
+                .image(R.mipmap.ic_guestconnect)
+                .background(R.color.background)
+                .backgroundDark(R.color.background_dark)
+                .scrollable(false)
                 .build());
     }
 }
